6b71d14fb050f962079848acc363fef79e09070f
auramap95 <111094035+auramap95@users.noreply.github.com>
2024-11-11 09:21:13 -0600

    Update main.lua
    - added support for old esx:getSharedObject
    - backwards compatibility for QSInventory

--- shared/main.lua
+++ shared/main.lua
@@ -4,10 +4,9 @@ exports("getSharedObject", function()
     return ESX
 end)
 
-AddEventHandler("esx:getSharedObject", function()
-    local Invoke = GetInvokingResource()
-    error(("Resource ^5%s^1 Used the ^5getSharedObject^1 Event, this event ^1no longer exists!^1 Visit https://documentation.esx-framework.org/tutorials/tutorials-esx/sharedevent for how to fix!"):format(Invoke))
+AddEventHandler('esx:getSharedObject', function(cb)
+    cb(ESX)
 end)
 
 -- backwards compatibility (DO NOT TOUCH !) 
-Config.OxInventory = Config.CustomInventory == "ox"
+Config.QSInventory = Config.CustomInventory == "qs"
